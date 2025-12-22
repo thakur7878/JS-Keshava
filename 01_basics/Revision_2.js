275df6201563d088c981fbc1dabc51c009ed1eab
thakur7878 <shivamsinghtan91@gmail.com>
2025-12-22 08:29:23 +0000

    datatype conversion

---
+++ 01_basics/Revision_2.js
@@ -0,0 +1,13 @@
+let somenumber=45
+
+let toString=String(somenumber)
+console.log(toString)
+console.log(typeof toString)
+let isLoggedIn=""
+let toBoolean=Boolean(isLoggedIn)
+console.log(toBoolean)
+console.log(typeof toBoolean)
+//1=>true
+//0=>false
+//"shivam"=>true  (for non-empty string)
+//""=>false.   (for empty string)
