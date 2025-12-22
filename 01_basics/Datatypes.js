7167c892b6d519e906a736b4ec6359e8d4ae1e75
thakur7878 <shivamsinghtan91@gmail.com>
2025-12-22 09:48:22 +0000

    Datatypes

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,31 @@
+//primitive
+
+//7 types: String, Number,Boolean,Null,undefined,Symbol,BigInt
+const score = 100
+scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol ('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId)
+
+const bigNumber=2334087248734823648
+console.log(bigNumber)
+
+
+
+//Reference (Non-primitive)
+
+//Arrays,Objects,Functions
+
+const warriors=["Arjun","Shwet","Abhimanyu"];
+let info={
+    name:"shivam",
+    city:"noida"
+
+}
+
+const myfunction=function(){
+    console.log("hare krsna");
+}
