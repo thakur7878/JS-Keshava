f0483847daf2f5cb5acd9513446f2f7e8116e526
Shivam Singh <shivamsinghtan91@gmail.com>
2026-01-04 19:27:20 +0530

    Arrays in js

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -20,9 +20,9 @@ console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("Shivam"))
+console.log(Array.from("Shivam"))
+console.log(Array.from({name: "Shivam"})) // interesting
 
 let score1 = 100
 let score2 = 200
