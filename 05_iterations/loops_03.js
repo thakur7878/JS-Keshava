0906ee0a36cec4bd7d82667748ee5643a7c0169b
Shivam Singh <shivamsinghtan91@gmail.com>
2026-02-02 22:13:09 +0530

    iterations2

---
+++ 05_iterations/loops_03.js
@@ -0,0 +1,26 @@
+
+
+// const arr=[1,2,3,4,5]
+
+// for(const num of arr){
+//     console.log(num)
+// }
+
+// const greetings="Hello World!"
+
+// for(const greet of greetings){
+//     console.log(`Each character is ${greet}`)
+// }
+
+const map=new Map()
+map.set('IN' , "India")
+map.set('USA' , "United States Of America")
+map.set('Fr' ,"France")
+map.set('IN' ,"india")
+
+console.log(map)
+
+for(const [key,value] of map){
+    console.log(key, ':-',value)
+}
+
